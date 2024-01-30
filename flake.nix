ccbabbd3f873ea895d82fca29588482fd4efe4f0
Robert W. Pearce <me@robertwpearce.com>
2024-01-30 10:37:08 -0500

    chore: bump GHC & flakes; rm zw3rk cache because it's not working

--- flake.nix
+++ flake.nix
@@ -6,11 +6,9 @@
     bash-prompt = "[hakyll-nix]λ ";
     extra-substituters = [
       "https://cache.iog.io"
-      "https://cache.zw3rk.com" # https://github.com/input-output-hk/haskell.nix/issues/1408
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
   };
 
@@ -25,7 +23,7 @@
           (final: prev: {
             hakyllProject = final.haskell-nix.project' {
               src = ./ssg;
-              compiler-nix-name = "ghc925";
+              compiler-nix-name = "ghc948";
               shell.buildInputs = [
                 hakyll-site
               ];
